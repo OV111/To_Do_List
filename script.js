49754a5f03f2afacc7edbd1edd3069fddedfdc48
Vahe <ohanyanv703@gmail.com>
2025-02-20 23:50:47 +0400

    adding js file with logic!

--- script.js
+++ script.js
@@ -1,24 +1,19 @@
 const addBtn = document.getElementById("btn");
 const inputText = document.getElementById("placeholder");
 
-let ul = document.getElementById("taskList");
+const ul = document.getElementById("taskList");
 
 function addingTask() {
     addBtn.addEventListener("click", () => {
         if(inputText.value.trim() !== "") {
             let li = document.createElement("li");
-            
+            let textContent = document.createTextNode(" " + inputText.value.trim());            
             let checkbox = document.createElement("input");
             checkbox.type = "checkbox";
             checkbox.className = "checkbox";
 
-            
-            let textContent = document.createTextNode(" " + inputText.value.trim());
-            
             li.appendChild(checkbox)
             li.appendChild(textContent);
-    
-
             ul.appendChild(li);
         } else {
             console.log("non string");
